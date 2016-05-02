e2b17facaa60e0105a597022542ff8484207fbec
Jamie Patel <jamie@notespublication.com>
2016-05-02 11:48:33 +0100

    Use global instead of window

--- src/client.js
+++ src/client.js
@@ -2,11 +2,12 @@
 
 const FetchMock = require('./fetch-mock');
 const statusTextMap = require('./status-text');
+const global = Function("return this")();
 
 module.exports = new FetchMock({
-	theGlobal: window,
-	Request: window.Request,
-	Response: window.Response,
-	Headers: window.Headers,
+	theGlobal: global,
+	Request: global.Request,
+	Response: global.Response,
+	Headers: global.Headers,
 	statusTextMap: statusTextMap
 });
